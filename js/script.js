9634b0aebbc049e66f50118db6cf00436e4dbbc7
Pobarobashka <pobarobashka@tut.by>
2021-06-28 20:01:19 +0300

    lesson5 function

--- js/script.js
+++ js/script.js
@@ -1,39 +1,30 @@
 "use strict"
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+function showFirstMessage(text){
+    console.log(text);
+}
 
-const personalMovieBD = {
-    count: numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:{},
-    privat:false
+showFirstMessage('Hello world');
+
+function calc(a,b){
+    return (a+b);
+}
+
+console.log(calc(4,3));
+console.log(calc(5,6));
+console.log(calc(6,10));
+
+function ret(){
+    let num = 50;
+    return num;
+}
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function (){
+  console.log('Hello');
 };
 
-// const a = prompt("Один из последних просмотренных фильмов",''),
-//     b = prompt('оцените его',''),
-//     c = prompt("Один из последних просмотренных фильмов",''),
-//     d = prompt('оцените его','');
+logger();
 
-for (let i = 0; i<2; i++){
-    const a = prompt("Один из последних просмотренных фильмов",''),
-        b = prompt('оцените его','');
-    if (a !=null && b!=null && a!='' && b !='' && a.length<50){
-        personalMovieBD.movies[a] = b;
-        console.log('done');
-    }else{
-        console.log('error');
-        i--;
-    }
-}
-if(personalMovieBD["count"]<10){
-    console.log("Просмотрено довольно мало фильмов");
-}else if (personalMovieBD["count"]<=30){
-    console.log("Вы классический зритель");
-}else {
-    console.log("Вы киноман");
-}
-
-
-
-console.log(personalMovieBD)
+const calc = (a,b) => {return  a + b};
