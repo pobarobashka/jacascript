f35eb3160816978a0e8a32993651c9de1be361a7
Pobarobashka <pobarobashka@tut.by>
2021-06-30 20:19:14 +0300

    lesson 7 array

--- js/script.js
+++ js/script.js
@@ -1,38 +1,30 @@
 "use strict"
+const arr = [1,2,3,5,6,8];
+arr.sort(compareNum);
 
-function learnJS(lang, callback){
-    console.log(`Я учу: ${lang}`);
-    callback();
+for (let i =0; i<arr.length; i++){
+    console.log(arr[i]);
 }
 
-function done(){
-    console.log('Я прошел урок!');
+for (let value of arr){
+    console.log(value);
 }
 
-learnJS('JavaScript', done);
+arr.forEach(function (num,i,arr){
+    console.log(`${i}: ${num} внутри ${arr}`);
+});
+// const str = prompt('','');
+// const products = str.split(",");
+// console.log(products);
+// const str = prompt('','');
+// const products = str.split(",");
+// console.log(products.join(';'));
+const str = prompt('','');
+const products = str.split(",");
+products.sort();
+console.log(products.join(';'));
 
-const options = {
-    name:'test',
-    width: 1024,
-    height:1024,
-    color:{
-        border:"black",
-        bg: 'red'
-    }
-};
-const {border, bg } = options["color"];
 
-delete  options.name;
-
-console.log(options);
-
-for (let key in options){
-    if (typeof(options[key]) ==='object'){
-        for (let i in options[key]){
-            console.log(`свойство ${i} имеет значение ${options[key][i]}`);
-        }
-        }else{
-        console.log(`свойство ${key} имеет значение ${options[key]}`);
-    }
+function compareNum(a,b){
+    return a-b;
 }
-console.log(Object.keys(options).length)
