8683c725448ae78f6e76a4db8f8e000bf687267a
Pobarobashka <pobarobashka@tut.by>
2021-07-01 12:21:22 +0300

    lesson 8 copy array and object

--- js/script.js
+++ js/script.js
@@ -1,30 +1,58 @@
 "use strict"
-const arr = [1,2,3,5,6,8];
-arr.sort(compareNum);
 
-for (let i =0; i<arr.length; i++){
-    console.log(arr[i]);
+// const copy = obj;
+//
+// copy.a = 10;
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj){
+    let objCopy = {};
+    let key;
+    for(key in mainObj){
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
 }
 
-for (let value of arr){
-    console.log(value);
+const number = {
+    a: 15,
+    b:5,
+    c:{
+        x:7,
+        y:9
+    }
+};
+
+const newNumber = copy(number);
+console.log(newNumber);
+
+function cons(mainObj){
+        for(let key in mainObj){
+       console.log(mainObj[key]);
+    }
+
 }
+cons(number);
+const add = {
+    d:17,
+    e:20
+};
 
-arr.forEach(function (num,i,arr){
-    console.log(`${i}: ${num} внутри ${arr}`);
-});
-// const str = prompt('','');
-// const products = str.split(",");
-// console.log(products);
-// const str = prompt('','');
-// const products = str.split(",");
-// console.log(products.join(';'));
-const str = prompt('','');
-const products = str.split(",");
-products.sort();
-console.log(products.join(';'));
+console.log(Object.assign(number,add));
 
+const oldArr=['a', 'b', 'c'];
+const newArr = oldArr.slice();
 
-function compareNum(a,b){
-    return a-b;
-}
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet =[...video, ...blogs, 'vk', 'facebook'];
+console.log(internet);
+
+const q ={
+    one: 1,
+    two: 2
+};
+
+const newQ = {...q};
+console.log(newQ);
