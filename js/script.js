0fb5a386c6478ff6223f8e038ee0d43d271334b4
Pobarobashka <pobarobashka@tut.by>
2021-07-01 12:44:15 +0300

    lesson 9 proto Object

--- js/script.js
+++ js/script.js
@@ -1,58 +1,16 @@
 "use strict"
 
-// const copy = obj;
-//
-// copy.a = 10;
-// console.log(copy);
-// console.log(obj);
-
-function copy(mainObj){
-    let objCopy = {};
-    let key;
-    for(key in mainObj){
-        objCopy[key] = mainObj[key];
-    }
-    return objCopy;
-}
-
-const number = {
-    a: 15,
-    b:5,
-    c:{
-        x:7,
-        y:9
-    }
+const soldier = {
+    heals: 400,
+    armor:100
 };
 
-const newNumber = copy(number);
-console.log(newNumber);
+// const jonh = {
+//     heals: 100
+// };
 
-function cons(mainObj){
-        for(let key in mainObj){
-       console.log(mainObj[key]);
-    }
+const jonh = Object.create(soldier);
 
-}
-cons(number);
-const add = {
-    d:17,
-    e:20
-};
-
-console.log(Object.assign(number,add));
-
-const oldArr=['a', 'b', 'c'];
-const newArr = oldArr.slice();
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blogger'],
-      internet =[...video, ...blogs, 'vk', 'facebook'];
-console.log(internet);
-
-const q ={
-    one: 1,
-    two: 2
-};
-
-const newQ = {...q};
-console.log(newQ);
+// Object.setPrototypeOf(jonh,soldier);
+// jonh.__proto__=soldier;
+console.log(jonh.armor);
