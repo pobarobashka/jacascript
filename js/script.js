9e147048ec9e6b56e969a8cbfb8cee610be6c940
Pobarobashka <pobarobashka@tut.by>
2021-07-02 11:17:34 +0300

    practice 2.1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 "use strict"
 
-
-
-
-// function start(){
-//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
-//     while (numberOfFilms=='' || numberOfFilms == null || isNaN(numberOfFilms)){
-//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
-//     }
-// }
-// start();
-
 const personalMovieBD = {
     count: '',
     movies:{},
@@ -58,12 +47,7 @@ const personalMovieBD = {
                 i--;
             }
         }
-        //     a[i-1]=prompt(`Ваш любимый жанр под номером ${i}`,'');
-        //     while (a[i-1]==='' || a[i-1] === null || isNaN(a[i-1])){
-        //         a[i-1]=prompt(`Ваш любимый жанр под номером ${i}`,'');
-        //     }
-        // }
-       a.forEach((item,i) => {
+        a.forEach(function (item,i){
            console.log(`Любимый жанр ${i+1} - это ${item}`);
         })
     },
@@ -71,50 +55,6 @@ const personalMovieBD = {
         personalMovieBD.privat = !personalMovieBD.privat;
     }
 };
-// let numberOfFilms = personalMovieBD.start();
-
-
-// function rememberMyFilms(){
-//     for (let i = 0; i<2; i++){
-//         const a = prompt("Один из последних просмотренных фильмов",''),
-//             b = prompt('оцените его','');
-//         if (a !=null && b!=null && a!='' && b !='' && a.length<50){
-//             personalMovieBD.movies[a] = b;
-//             console.log('done');
-//         }else{
-//             console.log('error');
-//             i--;
-//         }}
-// }
-// rememberMyFilms();
-
-
-// function detectedPersonalLevel(){
-//     if(personalMovieBD["count"]<10){
-//         console.log("Просмотрено довольно мало фильмов");
-//     }else if (personalMovieBD["count"]<=30){
-//         console.log("Вы классический зритель");
-//     }else {
-//         console.log("Вы киноман");
-//     }
-// }
-
-// detectedPersonalLevel();
-
-// function showMyDB(hidden){
-//     if (!hidden){
-//         console.log(personalMovieBD);
-//     }else {
-//         console.log('Нет доступа')
-//     }
-// }
-// function writeYourGenres(a){
-//     for (let i=1; i<=3; i++){
-//         a[i-1]=prompt(`Ваш любимый жанр под номером ${i}`,'');
-//     }
-// }
-// writeYourGenres(personalMovieBD.genres);
-// showMyDB(personalMovieBD["privat"]);
 
 personalMovieBD.start();
 // personalMovieBD.detectedPersonalLevel();
