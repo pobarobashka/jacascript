40383aefb6ebad8682f1cd8770fb6a13a56eb2f6
Pobarobashka <pobarobashka@tut.by>
2021-07-02 15:04:30 +0300

    tasks

--- js/script.js
+++ js/script.js
@@ -1,65 +1,19 @@
 "use strict"
 
-const personalMovieBD = {
-    count: '',
-    movies:{},
-    actors:{},
-    genres:[],
-    privat:true,
-    start:()=>{
-         personalMovieBD.count = +prompt('Сколько фильмов вы уже посмотрели?','');
-        while (personalMovieBD.count =='' || personalMovieBD.count == null || isNaN(personalMovieBD.count)){
-            personalMovieBD.count = +prompt('Сколько фильмов вы уже посмотрели?','');
-        }
-    },
-    rememberMyFilms(){
-        for (let i = 0; i<2; i++){
-            const a = prompt("Один из последних просмотренных фильмов",''),
-                b = prompt('оцените его','');
-            if (a !=null && b!=null && a!='' && b !='' && a.length<50){
-                personalMovieBD.movies[a] = b;
-                console.log('done');
-            }else{
-                console.log('error');
-                i--;
-            }}
-    },
-    detectedPersonalLevel(){
-        if(personalMovieBD["count"]<10){
-            console.log("Просмотрено довольно мало фильмов");
-        }else if (personalMovieBD["count"]<=30){
-            console.log("Вы классический зритель");
-        }else {
-            console.log("Вы киноман");
-        }
-    },
-    showMyDB(hidden){
-        if (!hidden){
-            console.log(personalMovieBD);
-        }else {
-            console.log('Нет доступа')
-        }
-    },
-    writeYourGenres:(a) => {
-        for (let i=1; i<=3; i++){
-            a[i-1]=prompt(`Ваш любимый жанр под номером ${i}`,'');
-            if( a[i-1]==='' ||  a[i-1] === null){
-                i--;
-            }
-        }
-        a.forEach(function (item,i){
-           console.log(`Любимый жанр ${i+1} - это ${item}`);
-        })
-    },
-    toggleVisibleMyDB(){
-        personalMovieBD.privat = !personalMovieBD.privat;
-    }
-};
+// let x = 5; alert( x++ );                      5
+// [ ] + false - null + true                 "NaN"
+// let y = 1; let x = y = 2; alert(x);      2
+//     [ ] + 1 + 2                          12
+// alert( "1"[0] )                          1
+//2 && 1 && null && 0 && undefined          null
+//!!( a && b ) и (a && b)                   false
+//alert( null || 2 && 3 || 4 );             3
+//a = [1, 2, 3]; b = [1, 2, 3]              false
+//alert( +"Infinity" );                     число Infinity
+//"Ёжик" > "яблоко"                         false
+//0 || "" || 2 || undefined || true || falsе    2
 
-personalMovieBD.start();
-// personalMovieBD.detectedPersonalLevel();
-// personalMovieBD.writeYourGenres(personalMovieBD.genres);
-personalMovieBD.toggleVisibleMyDB();
-personalMovieBD.writeYourGenres(personalMovieBD.genres);
-personalMovieBD.showMyDB(personalMovieBD.privat);
-// console.log(personalMovieBD);
+
+
+
+console.log(0 || "" || 2 || undefined || true || false);
