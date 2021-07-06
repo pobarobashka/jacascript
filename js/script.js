2983d97996a003afae6c44baabaa0ea7d7257c19
Pobarobashka <pobarobashka@tut.by>
2021-07-06 13:19:27 +0300

    lesson10 DOM

--- js/script.js
+++ js/script.js
@@ -1,19 +1,46 @@
-"use strict"
+'use strict';
 
-// let x = 5; alert( x++ );                      5
-// [ ] + false - null + true                 "NaN"
-// let y = 1; let x = y = 2; alert(x);      2
-//     [ ] + 1 + 2                          12
-// alert( "1"[0] )                          1
-//2 && 1 && null && 0 && undefined          null
-//!!( a && b ) и (a && b)                   false
-//alert( null || 2 && 3 || 4 );             3
-//a = [1, 2, 3]; b = [1, 2, 3]              false
-//alert( +"Infinity" );                     число Infinity
-//"Ёжик" > "яблоко"                         false
-//0 || "" || 2 || undefined || true || falsе    2
+const box = document.getElementById('box'),
+    buttons = document.getElementsByTagName('button'),
+    circles = document.getElementsByClassName('circle'),
+    wrapper = document.querySelector('.wrapper'),
+    hearts = wrapper.querySelectorAll('.heart'),
+    oneHeart = wrapper.querySelector('.heart');
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '300px';
+
+box.style.cssText = 'background-color:blue; width:500px'
+buttons[1].style.borderRadius = '20px';
+
+/*for (let i=0;i<hearts.length;i++){
+    hearts[i].style.backgroundColor = 'blue';
+}*/
+
+hearts.forEach(item=>{
+    item.style.backgroundColor = 'blue';
+})
+
+const div = document.createElement('div');
+// const  text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
 
 
 
+// wrapper.prepend(div);
+wrapper.append(div);
+// wrapper.appendChild(div);
+// hearts[0].before(div);
+// hearts[1].after(div);
+// wrapper.insertBefore(div,hearts[0]);
+// circles[1].remove();
+// wrapper.removeChild(hearts[1]);
+// hearts[1].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0],hearts[1]);
 
-console.log(0 || "" || 2 || undefined || true || false);
+// div.innerHTML= '<h1>Hello world</h1>';
+
+// div.textContent = 'hello!';
+
+div.insertAdjacentHTML('beforebegin','<h2>Hello</h2>');
