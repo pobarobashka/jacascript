3a814d3710062f772927d6cc42918ac2b1e1198a
Pobarobashka <pobarobashka@tut.by>
2021-07-10 15:13:32 +0300

    lesson12 DOM2

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-const btn = document.querySelector('button'),
-    overlay = document.querySelector('.overlay');
-// btn. onclick = function (){
-//     alert('Click');
-// };
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.body.childNodes);
+console.log(document.body.firstChild);
+console.log(document.body.firstElementChild);
+console.log(document.body.lastChild);
+console.log(document.body.lastElementChild);
+console.log(document.querySelector('#current').parentNode);
+console.log(document.querySelector('#current').parentElement);
+console.log(document.querySelector('#current').parentNode.parentNode);
+console.log(document.querySelector('[data-current="3"]').nextSibling);
+console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+console.log(document.querySelector('[data-current="3"]').previousSibling);
+console.log(document.querySelector('[data-current="3"]').previousElementSibling);
 
-btn.addEventListener('click',function (){
-    alert('Click');
-});
-let i =0;
-
-const q = (e)=>{
-    console.log(e.target, e.type);
-    i++;
-    if(i==1){
-        btn.removeEventListener('contextmenu',q);
+for(let node of document.body.childNodes){
+    if(node.nodeName =='#text'){
+        continue;
     }
-    // console.log('Hover');
-};
-btn.addEventListener('click',q);
-overlay.addEventListener('click',q);
-
-const link = document.querySelector('a');
-link.addEventListener('click',(e)=>{
-    e.preventDefault();
-    console.log(e.target);
-})
+    console.log(node);
+}
