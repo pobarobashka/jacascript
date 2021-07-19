9b2a107c8a9f29a02fc2218d05bb1b8baba5ec4e
Pobarobashka <pobarobashka@tut.by>
2021-07-19 13:35:27 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,36 +1,35 @@
+/*
 document.addEventListener('DOMContentLoaded',()=>{
+
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
 
     function hideTabsContent(){
         tabsContent.forEach((item)=>{
-            item.classList.add('hide');
-            item.classList.remove('show','fade');
+            item.classList.remove('active');
+            item.classList.add('hide')
         });
         tabs.forEach((item)=>{
             item.classList.remove('tabheader__item_active');
         });
     }
-    function showTabContent(i){
-        tabsContent[i].classList.add('show','fade');
+    function showTabsContent(i){
         tabsContent[i].classList.remove('hide');
+        tabsContent[i].classList.add('active','fade');
         tabs[i].classList.add('tabheader__item_active');
     }
-
     hideTabsContent();
-    showTabContent(0);
-
+    showTabsContent(0);
     tabsParent.addEventListener('click',(event)=>{
         const target = event.target;
-
-        if(target && target.classList.contains('tabheader__item')){
+        if (target && target.classList.contains('tabheader__item')){
             tabs.forEach((item,i)=>{
                 if (target==item){
                     hideTabsContent();
-                    showTabContent(i);
+                    showTabsContent(i);
                 }
             })
         }
     })
-})
+})*/
