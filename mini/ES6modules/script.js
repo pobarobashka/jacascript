887cc2983de222bbdec817c6d769e93e724b45c3
Pobarobashka <pobarobashka@tut.by>
2021-08-11 14:09:43 +0300

    lesson37 ES6modules

---
+++ mini/ES6modules/script.js
@@ -0,0 +1,7 @@
+// import {one as first, two} from "./main";
+
+// console.log(`${first} and ${two}`);
+// import sayHi from "./main";
+import * as data from './main'
+console.log(`${data.one} and ${data.two}`);
+data.sayHi();
