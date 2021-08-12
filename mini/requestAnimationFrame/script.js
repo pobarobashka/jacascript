fd1269f03267568069c93f53a8d20e9329ab1c64
Pobarobashka <pobarobashka@tut.by>
2021-08-12 15:33:49 +0300

    lesson40 requestAnimationFrame

---
+++ mini/requestAnimationFrame/script.js
@@ -0,0 +1,22 @@
+/*function myAnimation(){
+    let pos = 0;
+    const id = setInterval(frame, 10);
+    function frame(){
+        if(pos==300){
+            clearInterval(id);
+        }else{
+            pos++;
+            elem.style.top =`${pos}px`;
+            elem.style.left =`${pos}px`;
+        }
+    }
+}*/
+function myAnimation(){
+    pos++;
+    elem.style.top =`${pos}px`;
+    elem.style.left =`${pos}px`;
+    if (pos<300){
+        requestAnimationFrame(myAnimation)
+    }
+}
+btn.addEventListener('click',()=>requestAnimationFrame(myAnimation));
